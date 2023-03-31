eaa884fe64e37ef965d2fb137cb9c8b9a8669fc1
Monica <monica.a@kgisl.com>
2023-03-31 11:47:49 +0530

    first commit

---
+++ NlogWebAPI/NlogWebAPI/GenericHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace NlogWebAPI
+{
+    public class GenericHelper
+    {
+        private readonly ILogger<GenericHelper> _logger;
+        public GenericHelper(ILogger<GenericHelper> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation(1, "GenericHelper has been constructed");
+        }
+        public void JustADumbFunctionCall()
+        {
+            _logger.LogInformation("JustADumbFunctionCall has been called");
+        }
+    }
+}
